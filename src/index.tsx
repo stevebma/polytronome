41e824d742c98204cf355e0eb71c1fc518f2e069
Steven Bouma <steven@stevenbouma.nl>
2022-06-02 19:49:10 +0200

    introduce Redux wrapped app

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import store from './redux/store';
-import { Provider } from 'react-redux';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ReduxWrappedApp } from './ReduxWrappedApp';
 
 const rootElement = document.getElementById('app');
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	rootElement,
-);
+ReactDOM.render(<ReduxWrappedApp useDevTools={true} />, rootElement);
