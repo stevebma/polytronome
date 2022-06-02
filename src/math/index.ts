fcb0c500eea16cef7e521976b60f4ae3f0887460
Steven Bouma <steven@stevenbouma.nl>
2022-06-02 19:45:38 +0200

    adjust gcd and lcd functions

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,16 +1,16 @@
 // determine the greatest common divisor (GCD) of x and y
-Math.gcd = function(x: number, y: number): number {
-	x = Math.abs(x);
-	y = Math.abs(y);
-	while (y) {
-		const tmp: number = y;
-		y = x % y;
-		x = tmp;
-	}
-	return x;
+export const gcd = (x: number, y: number): number => {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while (y) {
+        const tmp: number = y;
+        y = x % y;
+        x = tmp;
+    }
+    return x;
 };
 
 // determine the least common multiple (LCM) of x and y
-Math.lcm = function(x: number, y: number): number {
-	return (x === 0 || y === 0) ? 0 : Math.abs((x * y) / Math.gcd(x, y));
+export const lcm = (x: number, y: number): number => {
+    return x === 0 || y === 0 ? 0 : Math.abs((x * y) / gcd(x, y));
 };
--- src/types/math.augmented.d.ts
+++
@@ -1,5 +0,0 @@
-// augment standard Math with additional functions
-interface Math {
-    gcd(x: number, y: number): number;
-    lcm(x: number, y: number): number;
-}
