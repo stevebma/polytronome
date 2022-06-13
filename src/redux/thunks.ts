83221f08e39d76e604f3e0a2870846ef1571d5be
Steven Bouma <steven@stevenbouma.nl>
2022-06-13 20:20:26 +0200

    modify Tone js imports

--- src/redux/thunks.ts
+++ src/redux/thunks.ts
@@ -1,5 +1,5 @@
 import type { AnyAction, ThunkAction } from '@reduxjs/toolkit';
-import { Channel, Destination, Sequence, Synth, Transport } from 'tone';
+import * as Tone from 'tone';
 
 import { selectCommonTimeSignature } from './selectors';
 import { startPlayback, stopPlayback } from './slices/playback';
@@ -19,7 +19,7 @@ export const toggle = (): AppThunk => (dispatch, getState) => {
 export const start = (): AppThunk => (dispatch, getState) => {
     const state = getState();
     if (!state.playback.isPlaying) {
-        if (Transport.state === 'started') {
+        if (Tone.Transport.state === 'started') {
             return;
         }
         // check if context is in suspended state (autoplay policy)
@@ -40,16 +40,16 @@ export const startAudio = (): AppThunk => (dispatch, getState) => {
     // private sourceB: Players;
 
     // master controls
-    Destination.volume.value = masterVolumeDb;
-    Destination.mute = isMuted;
+    Tone.Destination.volume.value = masterVolumeDb;
+    Tone.Destination.mute = isMuted;
 
     // layer0, panned left
-    const channel0: Channel = new Channel({ pan: -1, mute: isLayerMuted[0] }).toDestination();
+    const channel0: Tone.Channel = new Tone.Channel({ pan: -1, mute: isLayerMuted[0] }).toDestination();
     // layer1, panned right
-    const channel1: Channel = new Channel({ pan: 1, mute: isLayerMuted[1] }).toDestination();
+    const channel1: Tone.Channel = new Tone.Channel({ pan: 1, mute: isLayerMuted[1] }).toDestination();
     // const centered: Panner = new Panner(0).toDestination();
 
-    const beepA: Synth = new Synth({
+    const beepA: Tone.Synth = new Tone.Synth({
         oscillator: {
             // type: 'pulse',
             modulationType: 'sine',
@@ -66,7 +66,7 @@ export const startAudio = (): AppThunk => (dispatch, getState) => {
     beepA.envelope.attackCurve = 'exponential';
     beepA.envelope.releaseCurve = 'exponential';
 
-    const beepB: Synth = new Synth({
+    const beepB: Tone.Synth = new Tone.Synth({
         oscillator: {
             // type: 'pulse',
             modulationType: 'sine',
@@ -83,18 +83,18 @@ export const startAudio = (): AppThunk => (dispatch, getState) => {
     const commonTimeSignature = selectCommonTimeSignature(state);
 
     const bpmStretchFactor = commonTimeSignature.upper / layers[0].time.upper;
-    Transport.bpm.value = state.playback.bpm * bpmStretchFactor;
-    Transport.timeSignature = commonTimeSignature.toVector(); // e.g. [20, 4];
+    Tone.Transport.bpm.value = state.playback.bpm * bpmStretchFactor;
+    Tone.Transport.timeSignature = commonTimeSignature.toVector(); // e.g. [20, 4];
     // eslint-disable-next-line no-console
     console.log(`transforming to ${commonTimeSignature.toString()}`);
     const velocity = 0.35;
-    const sequences: Sequence<string>[] = layers.map(layer => {
+    const sequences: Tone.Sequence<string>[] = layers.map(layer => {
         const beep = layer.index === 0 ? beepA : beepB;
         const events: string[] = layer.transformTo(commonTimeSignature);
         // eslint-disable-next-line no-console
         console.dir(events.toString());
         const subdivision = '4n';
-        return new Sequence(
+        return new Tone.Sequence(
             (time, value: string) => {
                 if (value === '1') {
                     beep.triggerAttack('G5', time, velocity);
@@ -132,15 +132,15 @@ export const startAudio = (): AppThunk => (dispatch, getState) => {
     //     '4n',
     // ).start(0);
     /*********************************/
-    Transport.start();
+    Tone.Transport.start();
 };
 
 export const stop = (): AppThunk => (dispatch, getState) => {
     const state = getState();
     if (state.playback.isPlaying) {
         dispatch(stopPlayback());
-        if (Transport.state !== 'stopped') {
-            Transport.stop();
+        if (Tone.Transport.state !== 'stopped') {
+            Tone.Transport.stop();
         }
     }
 };
